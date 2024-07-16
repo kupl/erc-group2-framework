251a0c0c68db9fc50bcc2117ee3646008043b52e
오원석 <wonseok@owonseog-ui-iMac.local>
2024-07-16 09:35:46 +0900

    update

--- logger.py
+++ logger.py
@@ -3,14 +3,14 @@ import logging.handlers
 
 from rich.logging import RichHandler
 
-RICH_FORMAT = "[%(filename)s] >> %(message)s"
+RICH_FORMAT = ">> %(message)s"
 FILE_HANDLER_FORMAT = "[%(asctime)s]\\t%(levelname)s\\t[%(filename)s:%(funcName)s:%(lineno)s]\\t>> %(message)s"
 
 def set_logger(log_path) -> logging.Logger:
     logging.basicConfig(
         level="NOTSET",
         format=RICH_FORMAT,
-        handlers=[RichHandler(rich_tracebacks=True)]
+        handlers=[RichHandler(show_time=False, rich_tracebacks=True)]
     )
     logger = logging.getLogger("rich")
 
