915dbc2b33818b56537349893b63be03a1534ffe
오원석 <wonseok@iMac.local>
2025-04-09 16:35:34 +0900

    update

--- run.py
+++ run.py
@@ -8,11 +8,13 @@ from run_validator.run_validator import run as run_validator
 
 CUR_DIR = Path(os.getcwd())
 
-def run(src_dir, config, only_neg): 
-    run_test(config, only_neg)
-    if only_neg:
+def run(src_dir, config, only_test, patch_gen): 
+    if not patch_gen:
+        run_test(config)
+
+    if only_test:
         return
-        
+
     run_fault_localize(config)
 
     # src_dir = os.getcwd() + "/example/real/src"
@@ -29,11 +31,14 @@ def main() :
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--source-directory", dest="src_dir", action="store", required=True, type=Path)
     parser.add_argument("-c", "--config-file", dest="config", action="store", required=True, type=Path)
-    parser.add_argument("-n", "--only-neg", dest="only_neg", action="store_true")
+
+    additive_option = parser.add_mutually_exclusive_group()
+    additive_option.add_argument("-t", "--only-test", dest="only_test", action="store_true", help="Only run test casses")
+    additive_option.add_argument("-g", "--patch-gen", dest="patch_gen", action="store_true", help="Run patch generation")
 
     args = parser.parse_args()
 
-    run(args.src_dir, args.config, args.only_neg)
+    run(args.src_dir, args.config, args.only_test, args.patch_gen)
 
 if __name__ == "__main__" :
     main()
